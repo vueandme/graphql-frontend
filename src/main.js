340d9b071de3a6d298ea5b982dc70c4f53f68aba
ntepluhina <ntepluhina@gitlab.com>
2019-09-14 18:06:08 +0300

    Set up Apollo

--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
 import Vue from 'vue'
+import VueApollo from 'vue-apollo'
+import ApolloClient from 'apollo-boost'
 import App from './App.vue'
 import '@/assets/tailwind.css'
+
+Vue.use(VueApollo)
+
+const defaultClient = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+})
+
+const apolloProvider = new VueApollo({
+  defaultClient
+})
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  apolloProvider
 }).$mount('#app')
