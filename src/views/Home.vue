a8211862f5fd333d5f938e89ef9409574ffb5e90
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2019-09-29 14:54:49 +0200

    for gusto

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,21 @@
 <template>
   <div>
-    <h1 class="mt-4 text-black font-bold text-xl mb-2">
-      Here should be some wines...
-    </h1>
-    <div v-if="allWines.length" class="flex flex-wrap">
-      <WineCard v-for="wine in allWines" :key="wine.id" :wine="wine" />
-      <button
-        class="modal-open bg-transparent border border-gray-500 hover:border-main text-gray-500 hover:text-main font-bold py-2 px-4 rounded-full"
-        @click="modalOpen = true"
-      >
-        Add a new wine
-      </button>
-    </div>
+    <ApolloQuery :query="$options.allWinesQuery">
+      <template #default="{result: {error, data}, isLoading}">
+        <h1 v-if="isLoading" class="mt-4 text-black font-bold text-xl mb-2">
+          Loading
+        </h1>
+        <div v-if="data" class="flex flex-wrap">
+          <WineCard v-for="wine in data.allWines" :key="wine.id" :wine="wine" />
+        </div>
+      </template>
+    </ApolloQuery>
+    <button
+      class="modal-open bg-transparent border border-gray-500 hover:border-main text-gray-500 hover:text-main font-bold py-2 px-4 rounded-full"
+      @click="modalOpen = true"
+    >
+      Add a new wine
+    </button>
     <WineModal v-if="modalOpen" @close="closeModal">
       <template #form>
         <WineForm @submit="addNewWine" @cancel="closeModal" />
@@ -24,7 +28,11 @@
 import WineCard from '../components/WineCard'
 import WineModal from '../components/WineModal'
 import WineForm from '../components/WineForm'
+import allWinesQuery from '../graphql/allWines.query.gql'
+import addWineMutation from '../graphql/addWine.mutation.gql'
+
 export default {
+  allWinesQuery,
   components: {
     WineCard,
     WineModal,
@@ -33,11 +41,16 @@ export default {
   data() {
     return {
       // This array should be populated with data fetched from the API
-      allWines: [],
+      wines: [],
       modalOpen: false
     }
   },
   methods: {
+    fetchWines() {
+      this.$apollo.addSmartQuery('allWines', {
+        query: allWinesQuery
+      })
+    },
     checkButton(e) {
       if (e.keyCode === 27) {
         this.closeModal()
@@ -46,9 +59,17 @@ export default {
     closeModal() {
       this.modalOpen = false
     },
-    addNewWine(wine) {
+    async addNewWine(wine) {
       // The mutation logic for adding new wine should be written here
-      console.log(wine)
+      await this.$apollo.mutate({
+        mutation: addWineMutation,
+        variables: { wine },
+        update: (store, { data: { addWine } }) => {
+          const data = store.readQuery({ query: allWinesQuery })
+          data.allWines.push(addWine)
+          store.writeQuery({ query: allWinesQuery, data })
+        }
+      })
       this.closeModal()
     }
   },
--- src/views/Wine.vue
+++ src/views/Wine.vue
@@ -1,22 +1,66 @@
 <template>
-  <div class="w-full flex mt-8 border border-grey-light rounded">
-    <img src="../assets/wine.png" alt="Wine" class="wine-placeholder m-6" />
-    <div class="flex flex-col justify-between w-full my-5">
-      <h2 class="text-black font-bold text-xl mb-2 text-center">
-        Hello, I am wine with id {{ $route.params.id }}
-      </h2>
-      <p class="text-grey-darker text-base">
-        Let's fetch more details about me!
-      </p>
-      <button
-        type="button"
-        class="modal-close px-4 hover:bg-main bg-secondary py-3 rounded-lg text-white self-end mr-6"
+  <ApolloQuery
+    :query="$options.getWineQuery"
+    :variables="{ id: $route.params.id }"
+  >
+    <template #default="{result: {data, error}, isLoading}">
+      <div v-if="isLoading">
+        Loading
+      </div>
+      <div
+        v-if="!isLoading"
+        class="w-full flex mt-8 border border-grey-light rounded"
       >
-        Delete
-      </button>
-    </div>
-  </div>
+        <img src="../assets/wine.png" alt="Wine" class="wine-placeholder m-6" />
+        <div class="flex flex-col justify-between w-full my-5">
+          <h2 class="text-black font-bold text-xl mb-2 text-center">
+            Hello, I am wine with id {{ data.getWine.id }} Year:
+            <em>{{ data.getWine.year }}</em>
+          </h2>
+          <div>
+            From: {{ data.getWine.winery }} - {{ data.getWine.province }}
+          </div>
+          <p class="text-grey-darker text-base">
+            {{ data.getWine.description }}
+          </p>
+          <ApolloMutation
+            :mutation="$options.deleteWine"
+            :variables="{ id: $route.params.id }"
+            :update="rmFromCache"
+            @done="$router.replace('/')"
+          >
+            <template #default="{ mutate}">
+              <button
+                type="button"
+                class="modal-close px-4 hover:bg-main bg-secondary py-3 rounded-lg text-white self-end mr-6"
+                @click="mutate"
+              >
+                Delete
+              </button>
+            </template>
+          </ApolloMutation>
+        </div>
+      </div>
+    </template>
+  </ApolloQuery>
 </template>
+<script>
+import getWineQuery from '../graphql/getWine.query.gql'
+import allWinesQuery from '../graphql/allWines.query.gql'
+import deleteWine from '../graphql/deleteWine.mutation.gql'
+
+export default {
+  getWineQuery,
+  deleteWine,
+  methods: {
+    rmFromCache(store) {
+      const data = store.readQuery({ query: allWinesQuery })
+      data.allWines = data.allWines.filter(w => w.id !== this.$route.params.id)
+      store.writeQuery({ query: allWinesQuery, data })
+    }
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 .wine-placeholder {
